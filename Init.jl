1d467efb58bd275c67bcd7a50475055052675762
Samuel Neumann <samuelfneumann@gmail.com>
2021-11-16 06:39:56 -0700

    Add iterative and binary tree algorithms

--- Init.jl
+++ Init.jl
@@ -1,6 +1,9 @@
 using Random
+using DataStructures
 
 include("Grid.jl")
+
+@enum BiasDirection NW=1 NE=2 SW=3 SE=4
 
 """
     wilson!(g::Grid, rng::AbstractRNG)
@@ -58,10 +61,89 @@ function wilson_walk(free::Vector{Cell}, rng::AbstractRNG)::Vector{Cell}
     path
 end
 
+"""
+    binarytree!(g::Grid, rng::AbstractRNG, b::BiasDirection=NW)
+
+Initialize a grid with the binary tree maze initialization algorithm
+"""
+function binarytree!(g::Grid, rng::AbstractRNG, b::BiasDirection=NW)
+    f1, f2 = bias(b)
+
+    for r = 1:g.rows, c = 1:g.cols
+        cell = g[r, c]
+        neighbours::Vector{Cell} = []
+
+        if f1(cell) !== nothing
+            push!(neighbours, f1(cell))
+        end
+        if f2(cell) !== nothing
+            push!(neighbours, f2(cell))
+        end
+
+        if length(neighbours) > 0
+            index = abs((rand(rng, Int) % length(neighbours))) + 1
+            neighbourCell = neighbours[index]
+            link(cell, neighbourCell)
+        end
+    end
+end
+
+"""
+    iterative!(g::Grid, rng::AbstractRNG)
+
+Initialize a grid with the iterative maze initialization algorithm
+"""
+function iterative!(g::Grid, rng::AbstractRNG)
+    s = Stack{Cell}()
+    visited = Set{Cell}()
+
+    # Choose a random starting cell
+    r = abs(rand(rng, Int) % length(g.rows)) + 1
+    c = abs(rand(rng, Int) % length(g.cols)) + 1
+    currentCell = g[r, c]
+
+    push!(s, currentCell)
+
+    while length(s) > 0
+        # Choose a random unvisited neighbour
+        neighboursCells::Vector{Cell} = []
+        currentCell = pop!(s)
+        for cell in neighbours(currentCell)
+            if cell === nothing
+                continue
+            end
+
+            if !(cell in visited)
+                push!(neighboursCells, cell)
+            end
+        end
+
+        if length(neighboursCells) > 0
+            push!(s, currentCell)
+            index = abs(rand(rng, Int) % length(neighboursCells)) + 1
+            neighbourCell = neighboursCells[index]
+            link(neighbourCell, currentCell)
+            push!(visited, neighbourCell)
+            push!(s, neighbourCell)
+        end
+    end
+end
+
+function bias(b::BiasDirection)
+    if b == NW
+        return north, west
+    elseif b == NE
+        return north, east
+    elseif b == SW
+        return south, west
+    else
+        return south, east
+    end
+end
 
 g = Grid(10, 10)
 rng = Random.MersenneTwister(1)
-wilson!(g, rng)
+iterative!(g, rng)
 println(g)
 println(g[1, 2])
 println(g[1, 1].links)
